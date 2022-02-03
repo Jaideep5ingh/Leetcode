3530c9c3922f178e31cb486f32be3c742fe26496
jai01singh95@gmail.com <jai01singh95@gmail.com>
2022-02-03 16:09:44 -0500

    Added MiddleOfTheLinkedList876_FastPointer

--- src/Scratch.java
+++ src/Scratch.java
@@ -2,38 +2,22 @@ public class Scratch {
  //Definition for singly-linked list.
   public static void main(String[] args) {
    int length = 0;
-   int counter= 1;
-   ListNode sixth = new ListNode(1,null);
-   ListNode fifth = new ListNode(1,sixth);
-//   ListNode fifth = new ListNode(1,null);
+   int counter = 1;
 
-   ListNode fourth = new ListNode(2,fifth);
-   ListNode third = new ListNode(3,fourth);
-   ListNode second = new ListNode(4,third);
-   ListNode head = new ListNode(5,second);
+   ListNode sixth = new ListNode(1, null);
+   ListNode fifth = new ListNode(1, sixth);
+   ListNode fourth = new ListNode(2, fifth);
+   ListNode third = new ListNode(3, fourth);
+   ListNode second = new ListNode(4, third);
+   ListNode head = new ListNode(5, second);
 
-   ListNode.display(head);
-   length = length(head);
-   ListNode current = head ;
-   int middle_element = (length/2) +1;
+   ListNode fast = head;
+   ListNode slow = head;
 
-   while(counter <= middle_element){
-    if (counter == middle_element){
-     System.out.println("Middle Element : "  + current.val);
-     break;
-    }
-    counter++;
-    current= current.next;
+   while(fast != null && fast.next != null){
+    slow = slow.next;
+    fast = fast.next.next;
    }
-  }
-
-  public static int length(ListNode head){
-   ListNode current = head ;
-   int length = 0;
-   while(current!=null){
-    length++;
-    current= current.next;
-   }
-   return length;
+   System.out.println(slow.val);
   }
  }
