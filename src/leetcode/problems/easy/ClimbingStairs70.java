52754c644bba2282fdd1bba703b28507484dbfed
jai01singh95@gmail.com <jai01singh95@gmail.com>
2022-02-07 11:01:23 -0500

    Added ClimbingStairs70 using Fibonacci

--- src/leetcode/problems/easy/ClimbingStairs70.java
+++ src/leetcode/problems/easy/ClimbingStairs70.java
@@ -17,6 +17,8 @@ public class ClimbingStairs70 {
         int dp = dynamicProgramming(n, result);
         System.out.println("Number of combinations using DP: " + dp);
 
+        int fibo = fibonacciSeries(n, result);
+        System.out.println("Number of combinations using Fibo: " + fibo);
     }
 
     public static int dynamicProgramming(int n, int[] result){
@@ -25,5 +27,16 @@ public class ClimbingStairs70 {
             result[i] = result[i-1] +  result[i-2];
         }
         return result[n];
+    }
+
+    public static int fibonacciSeries(int n, int[] result){
+        int sum = 0;
+        int first = result[1], second = result[2], third = 0;
+        for (int i = 3; i <= n ; i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
     }
 }
