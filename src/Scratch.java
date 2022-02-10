7cacbb9cdb978faaa3a9b21c46c1895258a5585a
jai01singh95@gmail.com <jai01singh95@gmail.com>
2022-02-09 19:13:13 -0500

    Added MinCostClimbingStairs

--- src/Scratch.java
+++ src/Scratch.java
@@ -5,6 +5,18 @@ import java.util.*;
 
 public class Scratch {
  public static void main(String[] args) {
+  int[] cost = {4,2,1,2};
+
+  int minimumCost[] = new int[cost.length + 1];
+
+  // Start iteration from step 2, since the minimum cost of reaching
+  // step 0 and step 1 is 0
+  for (int i = 2; i < minimumCost.length; i++) {
+   int test = minimumCost[i - 1];
+   int takeOneStep = minimumCost[i - 1] + cost[i - 1];
+   int takeTwoSteps = minimumCost[i - 2] + cost[i - 2];
+   minimumCost[i] = Math.min(takeOneStep, takeTwoSteps);
+  }
 
  }
 }
