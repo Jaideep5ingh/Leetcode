3daa175d3755a0e1e182340f09a5c1a89b84187d
jai01singh95@gmail.com <jai01singh95@gmail.com>
2022-02-15 18:32:34 -0500

    Added code in RemoveNthNodeFromEndOfList19

--- src/Scratch.java
+++ src/Scratch.java
@@ -4,30 +4,7 @@ import java.util.*;
 
 public class Scratch {
  public static void main(String[] args) {
-  ListNode head1 = new ListNode(4);
-  ListNode l1first = new ListNode(1);
-  ListNode l1second = new ListNode(8);
-  ListNode l1third = new ListNode(4);
 
-  head1.next = l1first;
-  l1first.next = l1second;
-  l1second.next = l1third;
-
-  ListNode slow = new ListNode();
-  ListNode fast = new ListNode();
-
-
-  int n=3;
-
-  for (int i = 1; i <= n; i++) {
-   fast = fast.next;
-  }
-
-  while(fast!=null){
-   fast = fast.next;
-   slow= slow.next;
-  }
-  slow.next = slow.next.next;
  }
  }
 
