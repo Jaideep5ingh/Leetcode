cfa506aa7bd8df510d728c0680c85f7da91c038c
Jaideep5ingh <jai01singh95@gmail.com>
2022-08-03 02:33:24 -0400

    Added : Few additions to the unoin find file.

--- src/com/company/dataStructures/implementations/UnionFind.java
+++ src/com/company/dataStructures/implementations/UnionFind.java
@@ -17,6 +17,11 @@ public class UnionFind {
         }
     }
 
+//    TODO: Find function in quick find
+//    public static int find(int x){
+//        return root[x];
+//    }
+
     public int find(int x){
         if(x==root[x]){
             return x;
@@ -27,6 +32,34 @@ public class UnionFind {
     public boolean connected(int x, int y){
         return find(x)==find(y);
     }
+
+
+//    TODO: Simple Union function where we just set the parent of y as x
+//    TODO: Implementing “disjoint sets” - https://leetcode.com/explore/learn/card/graph/618/disjoint-set/3881/
+
+//    public static void union(int x, int y){
+//        int parentX = root[x];
+//        int parentY = root[y];
+//        root[y] = parentX; //Setting x as parent of y.
+//    }
+
+
+//    TODO: Union function in quick find
+//    public static void union(int x, int y){
+//        int parentX = find(x);
+//        int parentY = find(y);
+//        root[y] = parentX; //Setting x as parent of y.
+//        This step can be skipped as it will be taken care
+//        by the loop
+//
+//        //Extra Step
+//        for (int i = 0; i < root.length; i++){
+//            if (root[i] == parentY){
+//                root[i] = parentX;
+//            }
+//        }
+//    }
+
 
     public void union(int x, int y){
         int rootX= find(x);
