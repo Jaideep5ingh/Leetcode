c838bd44870d8a065c7b2ce7b6e0fb5e13ab30b8
Jaideep5ingh <jai01singh95@gmail.com>
2022-10-04 12:27:35 -0400

    Added My Kruskal Implementation in Scratch.java

--- src/Scratch.java
+++ src/Scratch.java
@@ -1,3 +1,6 @@
+import com.company.dataStructures.implementations.Kruskal;
+import com.company.dataStructures.implementations.UnionFind_PathCompression;
+
 import java.util.*;
 
 public class Scratch {
@@ -47,18 +50,56 @@ public class Scratch {
         }
     }
 
-    static int source = 0;
-    static int destination = 2;
-    static int n = 3;
-
     public static void main(String[] args) {
-        int[][] points = {{0,0},{2,2},{3,10},{5,2},{7,0}};
-        //create distance matrix
-        //sort each row of this matrix while iterating
-        //keep picking the smallest edge but would this guarantee a spanning tree?
-        
-
-
+        int[][] points = {{0, 0}, {2, 2}, {3, 10}, {5, 2}, {7, 0}};
+        Kruskal_my kruskalMy = new Kruskal_my();
+        System.out.print("Minimum Cost to Connect Points = ");
+        System.out.println(kruskalMy.minCostConnectPoints(points));
     }
 }
 
+class Kruskal_my{
+    protected int minCostConnectPoints(int[][] points){
+        int result = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>((x,y) -> x.cost - y.cost);
+        int size = points.length;
+        int count = size-1;
+        UnionFind_PathCompression unionFind = new UnionFind_PathCompression(size);
+
+        for (int i = 0; i < size; i++) {
+            int[] coordinates1 = points[i];
+            for (int j = 1; j < size; j++) {
+                int[] coordinates2 = points[j];
+                int cost = Math.abs((coordinates1[0] - coordinates2[0])) +
+                        Math.abs((coordinates1[1] - coordinates2[1]));
+                Edge edge = new Edge(i,j, cost);
+                pq.add(edge);
+            }
+        }
+
+        while(!pq.isEmpty() && count > 0){
+            Edge current = pq.poll();
+            int point1 = current.point1;
+            int point2 = current.point2;
+            if(!unionFind.connected(point1, point2)){
+                unionFind.union(point1, point2);
+                result+=current.cost;
+                count--;
+            }
+        }
+
+        return result;
+    }
+
+    class Edge{
+        int point1;
+        int point2;
+        int cost;
+
+        Edge(int point1, int point2, int cost){
+            this.point1 = point1;
+            this.point2 = point2;
+            this.cost = cost;
+        }
+    }
+}
