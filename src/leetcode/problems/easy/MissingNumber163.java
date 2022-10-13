a9ce7de642c1efd113319dbd5c3b40ba66e8dcab
Jaideep5ingh <jai01singh95@gmail.com>
2022-10-12 23:26:01 -0400

    Added MissingNumber163

---
+++ src/leetcode/problems/easy/MissingNumber163.java
@@ -0,0 +1,16 @@
+package leetcode.problems.easy;
+
+public class MissingNumber163 {
+    public static void main(String[] args) {
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums){
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i ^ nums[i];
+        }
+        return missing;
+    }
+}
