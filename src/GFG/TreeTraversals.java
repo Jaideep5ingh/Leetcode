21e6e4d5a18bf32d1eb7585e54d3a50f1ada622b
Jaideep5ingh <jai01singh95@gmail.com>
2022-11-13 18:32:27 -0500

    Added preOrder_Stack, postOrder, levelOrder, postOrder_Stack

--- src/GFG/TreeTraversals.java
+++ src/GFG/TreeTraversals.java
@@ -3,7 +3,7 @@ package GFG;
 import com.company.dataStructures.implementations.DsScratch;
 import com.company.dataStructures.implementations.TreeNode;
 
-import java.util.Stack;
+import java.util.*;
 
 
 public class TreeTraversals {
@@ -39,8 +39,56 @@ public class TreeTraversals {
         }
 
         System.out.print(root.val + " ");
-        inOrder_recursion(root.left);
-        inOrder_recursion(root.right);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    static void preOrder_Stack(TreeNode root){
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop();
+            System.out.print(current.val);
+            if (current.right!= null ) stack.push(current.right);
+            if (current.left!= null ) stack.push(current.left);
+        }
+    }
+
+    static void postOrder(TreeNode root){
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    static void postOrder_Stack(TreeNode root){
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+
+        s1.push(root);
+        while(!s1.isEmpty()){
+            TreeNode curr = s1.pop();
+            s2.push(curr);
+            if(curr.left!=null) s1.push(curr.left);
+            if(curr.right!=null) s1.push(curr.right);
+        }
+        while(!s2.isEmpty()){
+            System.out.print(s2.pop().val + " ");
+        }
+
+    }
+
+    static void levelOrder(TreeNode root){
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            TreeNode current = queue.poll();
+            System.out.print(current.val + " ");
+            if (current.left != null)queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
+        }
     }
 
     public static void main(String[] args) {
@@ -49,9 +97,17 @@ public class TreeTraversals {
         tree.root.left = new TreeNode(2);
         tree.root.right = new TreeNode(3);
         tree.root.left.left = new TreeNode(4);
+        tree.root.right.left = new TreeNode(6);
         tree.root.left.right = new TreeNode(5);
-        inOrder_recursion(tree.root);
+        tree.root.right.right = new TreeNode(7);
+//        inOrder_recursion(tree.root);
+//        System.out.println("");
+//        inOrder_Stack(tree.root);
+//        System.out.println("");
+//        preOrder(tree.root);
         System.out.println("");
-        inOrder_Stack(tree.root);
+//        levelOrder(tree.root);
+//        preOrder_Stack(tree.root);
+        postOrder_Stack(tree.root);
     }
 }
