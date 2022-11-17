605aa61615aee183d854e898a558cea611ccc190
Jaideep5ingh <jai01singh95@gmail.com>
2022-11-17 14:07:58 -0500

    Added correct symmetricTree_Iterative

---
+++ src/leetcode/problems/easy/SymmtericTree101.java
@@ -0,0 +1,42 @@
+package leetcode.problems.easy;
+
+import com.company.dataStructures.implementations.DsScratch;
+import com.company.dataStructures.implementations.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SymmtericTree101 {
+
+    static boolean symmetricTree_Iterative(TreeNode root){
+        Queue<TreeNode> stack = new LinkedList<>();
+        stack.offer(root);
+        stack.offer(root);
+        while(!stack.isEmpty()){
+            TreeNode node1 = stack.poll();
+            TreeNode node2 = stack.poll();
+            if(node1 == null && node2 == null) continue;
+            if(node1 == null || node2 == null) return false;
+            if(node1.val != node2.val) return false;
+            stack.offer(node1.left);
+            stack.offer(node2.right);
+            stack.offer(node1.right);
+            stack.offer(node2.left);
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        DsScratch tree = new DsScratch();
+        tree.root = new TreeNode(1);
+        tree.root.left = new TreeNode(2);
+        tree.root.right = new TreeNode(2);
+        tree.root.left.left = new TreeNode(3);
+        tree.root.right.left = new TreeNode(4);
+        tree.root.left.right = new TreeNode(4);
+        tree.root.right.right = new TreeNode(3);
+
+        System.out.println(symmetricTree_Iterative(tree.root));
+    }
+}
