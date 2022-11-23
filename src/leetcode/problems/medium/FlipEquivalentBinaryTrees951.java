7d7e9cf5fd63241759f44361ccf816e51f502203
Jaideep5ingh <jai01singh95@gmail.com>
2022-11-23 14:16:22 -0500

    Added improved FlipEquivalentBinaryTrees951

--- src/leetcode/problems/medium/FlipEquivalentBinaryTrees951.java
+++ src/leetcode/problems/medium/FlipEquivalentBinaryTrees951.java
@@ -5,10 +5,22 @@ import com.company.dataStructures.implementations.TreeNode;
 
 public class FlipEquivalentBinaryTrees951 {
     public static boolean flipEquiv(com.company.dataStructures.implementations.TreeNode root1, com.company.dataStructures.implementations.TreeNode root2) {
-        if (root1 == root2) return true;
-        if(root1 == null || root2 == null || root2.val != root1.val) return false;
-        return flipEquiv(root1.left, root2.left) & flipEquiv(root1.right, root2.right) ||
-                flipEquiv(root1.left, root2.right) & flipEquiv(root1.right, root2.left);
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val != root2.val) return false;
+
+        if (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) {
+            return true;
+        }
+
+        else if (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+
     }
 
 
