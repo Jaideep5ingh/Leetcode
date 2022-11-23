f5eb283fbfa5f47a509fda183a163f5c90913915
Jaideep5ingh <jai01singh95@gmail.com>
2022-11-23 14:12:18 -0500

    Added FlipEquivalentBinaryTrees951

---
+++ src/leetcode/problems/medium/FlipEquivalentBinaryTrees951.java
@@ -0,0 +1,36 @@
+package leetcode.problems.medium;
+
+import com.company.dataStructures.implementations.DsScratch;
+import com.company.dataStructures.implementations.TreeNode;
+
+public class FlipEquivalentBinaryTrees951 {
+    public static boolean flipEquiv(com.company.dataStructures.implementations.TreeNode root1, com.company.dataStructures.implementations.TreeNode root2) {
+        if (root1 == root2) return true;
+        if(root1 == null || root2 == null || root2.val != root1.val) return false;
+        return flipEquiv(root1.left, root2.left) & flipEquiv(root1.right, root2.right) ||
+                flipEquiv(root1.left, root2.right) & flipEquiv(root1.right, root2.left);
+    }
+
+
+    public static void main(String[] args) {
+        DsScratch tree = new DsScratch();
+        tree.root = new com.company.dataStructures.implementations.TreeNode(1);
+        tree.root.left = new com.company.dataStructures.implementations.TreeNode(3);
+        tree.root.right = new com.company.dataStructures.implementations.TreeNode(2);
+        tree.root.right.left = new com.company.dataStructures.implementations.TreeNode(5);
+
+        DsScratch tree2 = new DsScratch();
+        tree2.root = new com.company.dataStructures.implementations.TreeNode(1);
+        tree2.root.left = new com.company.dataStructures.implementations.TreeNode(2);
+        tree2.root.right = new com.company.dataStructures.implementations.TreeNode(3);
+        tree2.root.left.right = new TreeNode(5);
+
+        if (tree.root.val != tree2.root.val) {
+            System.out.println("Roots are unequal");
+            System.exit(0);
+        }
+
+        System.out.println(flipEquiv(tree.root, tree2.root));
+    }
+
+}
