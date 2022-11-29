b1022857097823d5a6b24fcba5a787dc3eb7628d
Jaideep5ingh <jai01singh95@gmail.com>
2022-11-28 23:59:55 -0500

    Added ProductOfArrayExceptSelf238

---
+++ src/leetcode/problems/medium/ProductOfArrayExceptSelf238.java
@@ -0,0 +1,33 @@
+package leetcode.problems.medium;
+
+public class ProductOfArrayExceptSelf238 {
+
+    public static  void productExceptSelf(int[] nums) {
+        int n = nums.length;
+//        given that the length of nums is atleast 2
+//        so not checking for num.length == 1
+        int[] leftAns = new int[n];
+        leftAns[0] = 1;
+
+        int[] rightAns = new int[n];
+        rightAns[n-1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            leftAns[i] = leftAns[i-1] * nums[i-1];
+        }
+
+        for (int i = n-2 ; i>=0; i--) {
+            rightAns[i] = rightAns[i+1] * nums[i+1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(leftAns[i] * rightAns[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        productExceptSelf(nums);
+    }
+
+}
