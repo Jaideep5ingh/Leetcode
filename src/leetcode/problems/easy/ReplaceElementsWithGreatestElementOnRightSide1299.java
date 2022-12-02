a5af021e22621c67cec7ae7abff7eebcf9c27394
Jaideep5ingh <jai01singh95@gmail.com>
2022-12-01 23:55:24 -0500

    Added greatestOnRight_startingFromRight to ReplaceElementsWithGreatestElementOnRightSide1299

--- src/leetcode/problems/easy/ReplaceElementsWithGreatestElementOnRightSide1299.java
+++ src/leetcode/problems/easy/ReplaceElementsWithGreatestElementOnRightSide1299.java
@@ -14,4 +14,19 @@ public class ReplaceElementsWithGreatestElementOnRightSide1299 {
         arr[n-1] = -1;
         for(int i : arr) System.out.print(i + " ");
     }
+
+    public int[] greatestOnRight_startingFromRight(int[] arr){
+        int n = arr.length;
+        if (n==1) return new int[]{-1}; //replace with return statement
+
+        int max = Integer.MIN_VALUE;
+        int[] res = new int[n];
+        res[n-1] = -1;
+
+        for (int i = n-2; i >=0 ; i--) {
+            max = Math.max(max, arr[i+1]);
+            res[i] = max;
+        }
+        return res;
+    }
 }
