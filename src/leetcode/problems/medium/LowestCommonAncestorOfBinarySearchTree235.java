6c0e1dbdd977bdda19674ac6fd62c0d6853d6498
Jaideep5ingh <jai01singh95@gmail.com>
2022-12-03 23:17:48 -0500

    Added greatestOnRight_startingFromRight to LowestCommonAncestorOfBinarySearchTree235

---
+++ src/leetcode/problems/medium/LowestCommonAncestorOfBinarySearchTree235.java
@@ -0,0 +1,17 @@
+package leetcode.problems.medium;
+
+import com.company.dataStructures.implementations.TreeNode;
+
+public class LowestCommonAncestorOfBinarySearchTree235 {
+    public static TreeNode lowestCommonAncestorBST(TreeNode root, TreeNode p, TreeNode q){
+        int rootVal= root.val;
+        int pVal = p.val;
+        int qVal = q.val;
+
+        if(qVal < rootVal && pVal < rootVal){
+            return lowestCommonAncestorBST(root.left, p, q);
+        }else if(qVal < rootVal && pVal > rootVal){
+            return lowestCommonAncestorBST(root.right, p, q);
+        }else return root;
+    }
+}
