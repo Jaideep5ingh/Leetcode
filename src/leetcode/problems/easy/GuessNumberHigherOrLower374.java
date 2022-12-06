098d740557fb5008596aa5b29d8dee1348f29a4c
Jaideep5ingh <jai01singh95@gmail.com>
2022-12-06 00:14:03 -0500

    Added GuessNumberHigherOrLower374

---
+++ src/leetcode/problems/easy/GuessNumberHigherOrLower374.java
@@ -0,0 +1,27 @@
+package leetcode.problems.easy;
+
+public class GuessNumberHigherOrLower374 {
+    public static int guessNumber(int n){
+        int mid = -1;
+        int left = 1, right = n;
+        while(left<=right){
+            mid = (left + right) >>> 1;
+            int res = guess(mid+1);
+            if(res == 0) return mid;
+            if(res == 1) left = mid + 1 ;
+            if(res == -1) right = mid -1 ;
+        }
+        return -1;
+    }
+
+//    Placeholder function to make things work
+    private static int guess(int i) {
+        return (int)(Math.random() + 10)/1;
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        int index = guessNumber(n) ;
+        System.out.println(index);
+    }
+}
