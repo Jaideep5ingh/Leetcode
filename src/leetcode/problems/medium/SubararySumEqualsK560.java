c2092bc67e5c4f06c97cba17d43803a28c7140ee
Jaideep5ingh <jai01singh95@gmail.com>
2022-12-07 16:58:24 -0500

    Added SubararySumEqualsK560 correct solutions

--- src/leetcode/problems/medium/SubararySumEqualsK560.java
+++ src/leetcode/problems/medium/SubararySumEqualsK560.java
@@ -1,21 +1,43 @@
 package leetcode.problems.medium;
 
+import java.util.HashMap;
+
 public class SubararySumEqualsK560 {
-    public static void main(String[] args) {
-        int[] nums = {3,4,7,2,-3,1,4,2};
-        int k = 7;
-        int sum = 0,count = 0;
-        for (int i = 0,j=0; i < nums.length && j < nums.length; j++) {
-            sum += nums[j];
-            if(sum >= k){
-                if(sum == k){
-                    count++;
+    public static int subarraySum(int[] nums, int k){
+        int count = 0, sum=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(sum,1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum,0) + 1);
+        }
+        return count;
+    }
+
+    public static int subarraySum_cummulativeSum(int[] nums, int k){
+        int count = 0, sum=0;
+        int[] sums = new int[nums.length + 1];
+        sums[0] = 0;
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i-1] + nums[i-1];
+            for (int start = 0; start < nums.length; start++) {
+                for (int end = start+1; end <= nums.length ; end++) {
+                    if(sums[end] - sums[start] == k )
+                        count++;
                 }
-                i++;
-                j=i-1;
-                sum = 0;
             }
         }
-        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,6};
+        int k = 6;
+        System.out.println(subarraySum(nums, k));
+        System.out.println(subarraySum_cummulativeSum(nums, k));
     }
 }
+
+//54189456
