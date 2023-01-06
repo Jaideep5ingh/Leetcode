131340b077450924ed4e6f36aff84b8f9ef154fa
Jaideep5ingh <jai01singh95@gmail.com>
2023-01-05 21:48:53 -0500

    Added guessNumber_TernarySearch in GuessNumberHigherOrLower374

--- src/leetcode/problems/easy/GuessNumberHigherOrLower374.java
+++ src/leetcode/problems/easy/GuessNumberHigherOrLower374.java
@@ -14,6 +14,22 @@ public class GuessNumberHigherOrLower374 {
         return -1;
     }
 
+    public static int guessNumber_TernarySearch(int n){
+        int left = 1, right = n;
+        while(left<=right){
+            int mid1 = left + (right - left)/3;
+            int mid2 = right - (right - left)/3;
+            if(guess(mid1) == 0 ) return mid1;
+            if(guess(mid2) == 0 ) return mid2;
+            else if(guess(mid1) == -1) right = mid1 - 1;
+            else if(guess(mid2) == 1) left = mid2 + 1;
+            else {
+                left = mid1 + 1;
+                right = mid2 - 1;
+            }
+        }
+        return -1;
+    }
 //    Placeholder function to make things work
     private static int guess(int i) {
         return (int)(Math.random() + 10)/1;
@@ -23,5 +39,6 @@ public class GuessNumberHigherOrLower374 {
         int n = 12;
         int index = guessNumber(n) ;
         System.out.println(index);
+        System.out.println(guessNumber_TernarySearch(n));
     }
 }
