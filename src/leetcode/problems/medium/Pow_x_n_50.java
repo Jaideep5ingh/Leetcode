8821f9736f5ab47a7d197731efebcee28d15b9ee
Jaideep5ingh <jai01singh95@gmail.com>
2023-01-08 19:50:05 -0500

    Added myPow_iterative - binary search

--- src/leetcode/problems/medium/Pow_x_n_50.java
+++ src/leetcode/problems/medium/Pow_x_n_50.java
@@ -24,4 +24,22 @@ public class Pow_x_n_50 {
             return half * half * x;
         }
     }
+    //This is a concept related to binary search since it cuts the space
+    //in half at every step - half i in the loop. No need of mid here
+    // as the basic concept behind binary search is cutting the space in
+    //half running the program in log n time.
+    public static double myPow_iterative(double x, int n) {
+        if(x==0) return 1.0;
+        if(n<0) {
+            x = 1/x;
+            n = Math.abs(n);//to convert negative value of n to positive.
+        }
+        double product=1.0;
+        double current_product = x;
+        for(int i = n; i > 0 ; i/=2){
+            if(i%2==1) product= product * current_product;
+            current_product = current_product * current_product;
+        }
+        return product;
+    }
 }
