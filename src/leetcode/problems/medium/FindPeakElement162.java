b1df151cb758e2b7f50398e9764f2b2d826756eb
Jaideep5ingh <jai01singh95@gmail.com>
2023-01-12 10:22:45 -0500

    Added FindPeakElement162 - leetcode recursive and iterative

--- src/leetcode/problems/medium/FindPeakElement162.java
+++ src/leetcode/problems/medium/FindPeakElement162.java
@@ -19,6 +19,27 @@ public class FindPeakElement162 {
 //        return rightSubArray!=-1 ? rightSubArray : (leftSubArray!=-1 ? leftSubArray : -1);
     }
 
+    public static int findPeakElement_leetcodeRecursive(int[] nums, int left, int right) {
+        if(left==right) //I think we don't need to check left < right since we are always moving towards the center
+                        //and we aren't making right = mid-1 rather right = mid, right might not overshoot left
+            return left;
+        int mid = (left + right) >> 1;
+        if(nums[mid] > nums[mid+1])
+            return findPeakElement_leetcodeRecursive(nums, left, mid);
+        else return findPeakElement_leetcodeRecursive(nums, mid+1, right);
+    }
+
+    public static int findPeakElement_leetcodeIterative(int[] nums) {
+        int n = nums.length;
+        int left = 0,  right = n-1;
+        while(left<right){
+            int mid = (left + right) >> 1;
+            if(nums[mid] > nums[mid+1]) right = mid;
+            else left = mid-1;
+        }
+        return left;
+    }
+
     public static void main(String[] args) {
 //        int[] nums4 = {1,2,1,3,5,6,4}; //mid = 6, target = 4
         int[] nums4 = {1,2}; //mid = 6, target = 4
