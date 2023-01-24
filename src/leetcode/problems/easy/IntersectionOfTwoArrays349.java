6c04541f486d8968639f2bf5701077601fbf55df
Jaideep5ingh <jai01singh95@gmail.com>
2023-01-23 23:51:11 -0500

    Added IntersectionOfTwoArrays349 intersection_countSort

--- src/leetcode/problems/easy/IntersectionOfTwoArrays349.java
+++ src/leetcode/problems/easy/IntersectionOfTwoArrays349.java
@@ -28,9 +28,36 @@ public class IntersectionOfTwoArrays349 {
         return Arrays.copyOf(result,j);
     }
 
+    public static int[] intersection_countSort(int[] nums1, int[] nums2) {
+        int nums1Length = nums1.length;
+        int nums2Length = nums2.length;
+
+        //Always treating nums1 as the shorter array.
+        if(nums1Length  > nums2Length) return intersection(nums2, nums1);
+
+        int[] countSort = new int[1001];
+        for(int num : nums1) countSort[num] = 1;
+
+        int[] result = new int[nums1Length];
+
+        // this will be the last index in result where we insert an element which
+        // exists both in nums1 and nums2
+        int lastIndex=0;
+
+        for (int i = 0; i < nums2Length; i++) {
+            int num = nums2[i];
+            if(countSort[num] == 1){
+                result[lastIndex] = num;
+                countSort[num] = 0;
+                lastIndex++;
+            }
+        }
+        return Arrays.copyOf(result,lastIndex);
+    }
+
     public static void main(String[] args) {
-        int[] nums1 = {1,2,2,1}; //mid = 7, target = 1
-        int[] nums2 = {2,2}; //mid = 7, target = 1
-        System.out.println(Arrays.toString(intersection(nums1,nums2)));
+        int[] nums1 = {4,9,5}; //mid = 7, target = 1
+        int[] nums2 = {9,4,9,8,4}; //mid = 7, target = 1
+        System.out.println(Arrays.toString(intersection_countSort(nums1,nums2)));
     }
 }
