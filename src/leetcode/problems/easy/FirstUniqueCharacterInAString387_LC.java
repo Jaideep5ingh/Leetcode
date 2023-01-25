3f6622849903e2a1408ffb5cb32562a5701607f8
Jaideep5ingh <jai01singh95@gmail.com>
2023-01-25 01:19:00 -0500

    Added firstUniqChar_array

--- src/leetcode/problems/easy/FirstUniqueCharacterInAString387_LC.java
+++ src/leetcode/problems/easy/FirstUniqueCharacterInAString387_LC.java
@@ -1,6 +1,9 @@
 package leetcode.problems.easy;
 
+import javafx.util.Pair;
+
 import java.util.HashMap;
+import java.util.Map;
 
 public class FirstUniqueCharacterInAString387_LC {
     public static void main(String[] args) {
@@ -12,14 +15,28 @@ public class FirstUniqueCharacterInAString387_LC {
             char c = s.charAt(i);
             count.put(c, count.getOrDefault(c, 0) + 1);
         }
-
+        System.out.println(firstUniqChar_array(s));
         // find the index
         for (int i = 0; i < n; i++) {
             if (count.get(s.charAt(i)) == 1) {
                 System.out.println(i);
                 System.exit(0);
             }
-
         }
+
+    }
+
+    public static int firstUniqChar_array(String s) {
+        int[] frequency = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            frequency[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(frequency[c - 'a'] == 1) return i;
+        }
+        return -1;
     }
 }
