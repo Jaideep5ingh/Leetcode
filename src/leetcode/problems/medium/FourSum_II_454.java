b0463f35731251dfa215f09462ea057fe12f6dbc
Jaideep5ingh <jai01singh95@gmail.com>
2023-01-29 17:27:45 -0500

    Added FourSum_II_454 added generalized solution

--- src/leetcode/problems/medium/FourSum_II_454.java
+++ src/leetcode/problems/medium/FourSum_II_454.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FourSum_II_454 {
+    static int[][] lists;
+
     public static int fourSumCount_hashMap(int[] A, int[] B, int[] C, int[] D) {
         int count = 0;
         Map<Integer, Integer> map = new HashMap<>();
@@ -16,6 +18,33 @@ public class FourSum_II_454 {
             for(int d : D){
                 count+=map.getOrDefault(-(c+d),0);
             }
+        }
+        return count;
+    }
+
+    public static int fourSumCount_generalized_solution(int[] A, int[] B, int[] C, int[] D) {
+        lists =  new int[][]{A, B, C, D};
+        int k = lists.length;
+        Map<Integer, Integer> left = sumCount(0, k/2);
+        Map<Integer, Integer> right = sumCount(k/2, k);
+        int res = 0;
+        for(int s : left.keySet()){
+            res = res + (left.get(s) * right.getOrDefault(-s, 0));
+        }
+        return res;
+    }
+
+    public static Map<Integer, Integer> sumCount(int start, int end){
+        Map<Integer, Integer> count = new HashMap<>();
+        count.put(0,1);
+        for (int i = start; i < end; i++) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int a : lists[i]){
+                for(int total : count.keySet()){
+                    map.put(total + a, map.getOrDefault(total + a, 0) + count.get(total));
+                }
+            }
+            count = map;
         }
         return count;
     }
