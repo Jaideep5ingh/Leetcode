3bbb673e9dffd03ba0d836d6b7c3b44b2f19e0fe
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-01 13:40:34 -0500

    Added strStr_my

--- src/leetcode/problems/medium/FindTheIndexOfTheFirstOccurrenceInString28.java
+++ src/leetcode/problems/medium/FindTheIndexOfTheFirstOccurrenceInString28.java
@@ -14,6 +14,26 @@ public class FindTheIndexOfTheFirstOccurrenceInString28 {
         return -1;
     }
 
+    public static int strStr_my(String haystack, String needle) {
+        int hLength = haystack.length(), nLength = needle.length();
+        if(nLength > hLength) return -1;
+        int left = 0, right = 0;
+        while(left < hLength && right < nLength){
+            char hayChar = haystack.charAt(left);
+            if(hayChar != needle.charAt(right)) {
+                right = 0;
+//                while(left < hLength && haystack.charAt(left) == haystack.charAt(left+1)) left++;
+                if (left > 0 && haystack.charAt(left-1) == needle.charAt(0) && haystack.charAt(left-1)!=haystack.charAt(left)) {
+                    left--;
+                    continue;
+                }
+            }
+            else ++right;
+            ++left;
+        }
+        return right == nLength? left - nLength : -1;
+    }
+
     public static void main(String[] args) {
         String haystack = "ppi" , needle = "pi";
         System.out.println(strStr_slidingWindow(haystack, needle));
