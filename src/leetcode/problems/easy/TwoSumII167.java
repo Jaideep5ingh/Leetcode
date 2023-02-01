41d66d52ae4a1a7efbc1e975ea59fdcffbb1158f
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-01 13:40:11 -0500

    Added improved code.

--- src/leetcode/problems/easy/TwoSumII167.java
+++ src/leetcode/problems/easy/TwoSumII167.java
@@ -1,19 +1,27 @@
 package leetcode.problems.easy;
 
+import java.util.Arrays;
+
 public class TwoSumII167 {
+
     public static void main(String args[]) {
-        int[] numbers = {2,7,11,15};
-        int target = 9;
-        int i = 0, j= numbers.length-1;
-        while (i<=j){
-            if(numbers[i] + numbers[j] == target){
-                System.out.println("Requireed indices are - " + i + " and " + j);
+        int[] nums2 = {4,6,7,77,234,7854}; int target=84;
+        System.out.println(Arrays.toString(twoSum(nums2, target)));
+    }
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length-1;
+        int[] result = new int[]{-1,-1};
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+            if(sum == target){
+                result[0] = left+1;
+                result[1] = right+1;
                 break;
-            }else if(numbers[i] + numbers[j] > target){
-                j--;
-            }else if(numbers[i] + numbers[j] < target){
-                i++;
             }
+            if(sum<target) ++left;
+            else --right;
         }
+        return result;
     }
 }
