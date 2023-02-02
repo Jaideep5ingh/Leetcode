cac2644c035d51554c7294a5a7daf2e904bf567c
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-02 16:15:20 -0500

    Added extra array version

--- src/leetcode/problems/medium/RotateArrays189.java
+++ src/leetcode/problems/medium/RotateArrays189.java
@@ -20,4 +20,14 @@ public class RotateArrays189 {
             lastIndex--;
         }
     }
+
+    public static void reverse_extra_Array(int[] nums, int k){
+        int n = nums.length;
+        if(k>n) k%=n;
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[(i+k) % n] = nums[i];
+        }
+        for (int i = 0; i < n; i++) nums[i] = result[i];
+    }
 }
