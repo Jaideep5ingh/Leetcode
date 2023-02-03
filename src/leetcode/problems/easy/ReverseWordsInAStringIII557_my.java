ff0dfb651065b5175f865abc3f4699f9d96ab69e
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-03 00:14:56 -0500

    Added ReverseWordsInAStringIII557 NEW SOLUTIONS

--- src/leetcode/problems/easy/ReverseWordsInAStringIII557_my.java
+++ src/leetcode/problems/easy/ReverseWordsInAStringIII557_my.java
@@ -11,7 +11,7 @@ public class ReverseWordsInAStringIII557_my {
         StringBuilder result = new StringBuilder();
 
         for (String word : string){
-            char[] characterArray = convertToChar(word);
+            char[] characterArray = word.toCharArray();
             result.append(reverse(characterArray,characterArray.length-1) + " ");
         }
         System.out.println(result.toString().trim());
@@ -29,12 +29,56 @@ public class ReverseWordsInAStringIII557_my {
         System.out.println("Reverse word : " + Arrays.toString(s));
         return Arrays.toString(s);
     }
-
-    public static char[] convertToChar(String s){
-        char[] characterArray = new char[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            characterArray[i] = s.charAt(i);
-        }
-        return characterArray;
+    public static String reverse(String s){
+        StringBuilder result = new StringBuilder();
+        for (int i = s.length()-1; i >= 0; i--) result.append(s.charAt(i));
+        return result.toString();
     }
+
+    public static String reverseWords_leetcode(String s) {
+        StringBuilder result = new StringBuilder();
+        int lastSpaceIndex = -1;
+        for (int currentIndex = 0; currentIndex <= s.length(); currentIndex++) {
+            if(currentIndex == s.length() || s.charAt(currentIndex) == ' '){
+                int start = lastSpaceIndex+1;
+                result.append(reverse(s.substring(start, currentIndex)));
+                if(currentIndex!=s.length()) result.append(" ");
+                lastSpaceIndex = currentIndex;
+            }
+        }
+        return result.toString();
+    }
+
+    //TODO : INSPIRED BY 151. Reverse Words in a String
+    public static String reverseWords_my(String s) {
+        int left = 0, right = s.length()-1;
+        StringBuilder leftString = new StringBuilder();
+        StringBuilder rightString = new StringBuilder();
+
+        while(s.charAt(left)==' ' && left<=right) {
+            leftString.append(" ");
+            ++left;
+        }
+        while(s.charAt(right)==' ' && left<=right) {
+            rightString.append(" ");
+            --right;
+        }
+
+        Stack<String> queue = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        while(left<=right){
+            char c = s.charAt(left);
+            if(result.length()!=0 && c == ' '){
+                queue.add(reverse(result.toString()) + " ");
+                result.setLength(0);
+
+            }else if (c!=' ') result.append(c);
+            ++left;
+        }
+        queue.add(reverse(result.toString()));
+        String str = String.join("", queue);
+        return leftString + str + rightString;
+    }
+
 }
