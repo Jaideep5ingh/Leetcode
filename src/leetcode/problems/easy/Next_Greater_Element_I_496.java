4ab323215d2dbbaa8304bf6ea8850bed95f3b46c
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-03 19:05:35 -0500

    Added Next_Greater_Element_I_496

---
+++ src/leetcode/problems/easy/Next_Greater_Element_I_496.java
@@ -0,0 +1,30 @@
+package leetcode.problems.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Next_Greater_Element_I_496 {
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        int len = nums2.length;
+        for (int i = 0; i < nums2.length; i++) {
+            while(!stack.isEmpty() && stack.peek() < nums2[i]){
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        while(!stack.isEmpty()) map.put(stack.pop(), -1);
+        for (int i = 0; i < nums1.length; i++) nums1[i] =map.get(nums1[i]) ;
+        return nums1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        System.out.println(Arrays.toString(nextGreaterElement(nums, nums2)));
+    }
+}
