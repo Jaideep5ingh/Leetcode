06fd5b3dde450996e54d3ef9a2b174c9bc45e0ad
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-05 20:24:55 -0500

    Added ValidPalindromeII680 validPalindrome_MY

--- src/leetcode/problems/easy/ValidPalindromeII680.java
+++ src/leetcode/problems/easy/ValidPalindromeII680.java
@@ -22,4 +22,26 @@ public class ValidPalindromeII680 {
         }
         System.out.println("String can be a palindrome by remove at most one character");
     }
+
+    //TODO : DOES NOT WORK. JUST RECORDING MY ATTEMPT
+    public static boolean validPalindrome_MY(String s) {
+        if(s.length() == 1) return true;
+        boolean charRemoved = false;
+        int left = 0, right = s.length()-1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
+                if(charRemoved) return false;
+                if(left - right  + 1== 0 ) return true;
+                else if(s.charAt(left+1) == s.charAt(right)) {
+                    charRemoved = true;
+                    ++left;
+                }else if (s.charAt(left) == s.charAt(right-1)) {
+                    charRemoved = true;
+                    --right;
+                }
+                else return false;
+            }else ++left; --right;
+        }
+        return true;
+    }
 }
