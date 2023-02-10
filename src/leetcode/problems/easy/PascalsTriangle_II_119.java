8139217f24f3d6a59af9eb5cdc1df28722e2565a
Jaideep5ingh <jai01singh95@gmail.com>
2023-02-10 15:28:48 -0500

    Added recursive solutions in PascalsTriangle_II_119

--- src/leetcode/problems/easy/PascalsTriangle_II_119.java
+++ src/leetcode/problems/easy/PascalsTriangle_II_119.java
@@ -1,6 +1,7 @@
 package leetcode.problems.easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PascalsTriangle_II_119 {
@@ -35,15 +36,47 @@ public class PascalsTriangle_II_119 {
         return Row;
     }
 
+    public static List<Integer> getRow_my_recursion(int rowIndex) {
+        if(rowIndex == 0) return new ArrayList<>(Arrays.asList(1)) ;
+        return helper(new ArrayList<>(Arrays.asList(1,1)), rowIndex, 2);
+    }
+
+    public static List<Integer> helper(List<Integer> prevRow, int rowIndex, int curr){
+        if(curr > rowIndex) return prevRow;
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        for (int j = 1; j < prevRow.size(); j++) {
+            list.add(prevRow.get(j-1) + prevRow.get(j));
+        }
+        list.add(1);
+        return helper(list, rowIndex, curr+1);
+    }
+
+    public static List<Integer> getRow_leetcode_recursion(int rowIndex) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; i++) {
+            result.add(helper_leetcode(rowIndex, i));
+        }
+        return result;
+    }
+
+    public static int helper_leetcode(int row, int col){
+        if(row==0 || col == 0 || row == col) return 1;
+
+        return helper_leetcode(row-1, col-1) + helper_leetcode(row-1, col);
+    }
+
+
     public static void main(String[] args) {
 //        char[] jewels = {'t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e'};
 //        int[] nums = {0,0,1,1,1,2,2,3,3,4};
 //        String[] strs = {"flower","flow","flight"};
 //        int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
 //        char[] s = {'h','e','l','l','o'};
-        int n = 7;
-        System.out.println(getRow_my(n));
-        System.out.println(getRow_leetcode(n));
+        int n = 3;
+//        System.out.println(getRow_my(n));
+//        System.out.println(getRow_leetcode(n));
+        System.out.println(getRow_leetcode_recursion(n));
 
     }
 }
