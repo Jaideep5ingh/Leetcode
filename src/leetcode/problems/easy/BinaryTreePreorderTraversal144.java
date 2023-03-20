2044ab9dd18608167167c06f68dfecf617a335c6
Jaideep5ingh <jai01singh95@gmail.com>
2023-03-20 16:39:45 -0400

    Added preorderTraversal_iterative

--- src/leetcode/problems/easy/BinaryTreePreorderTraversal144.java
+++ src/leetcode/problems/easy/BinaryTreePreorderTraversal144.java
@@ -2,6 +2,7 @@ package leetcode.problems.easy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal144 {
     static List<Integer> result = new ArrayList<>();
@@ -16,6 +17,21 @@ public class BinaryTreePreorderTraversal144 {
         return result;
     }
 
+    public static List<Integer> preorderTraversal_iterative(TreeNode root) {
+        if(root == null) return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+//            result.add(node.val);
+            System.out.print(node.val + "--->");
+            if(node.right!=null) stack.push(node.right);
+            if(node.left!=null) stack.push(node.left);
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         TreeNode three = new TreeNode(3);
         TreeNode one = new TreeNode(1);
@@ -23,6 +39,7 @@ public class BinaryTreePreorderTraversal144 {
         TreeNode five = new TreeNode(5);
         TreeNode root = new TreeNode(4, two, five);
 
-        preorderTraversal(root);
+//        preorderTraversal(root);
+        preorderTraversal_iterative(root);
     }
 }
